9c3304b58a51c47e422fd1cca611b98465dea4ca
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-23 19:05:55 -0400

    sound plays

--- HowsMatt/ViewController.swift
+++ HowsMatt/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+
+    @IBOutlet weak var stepperPressed: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func playsound(soundName : String, audioPlayer: inout AVAudioPlayer) {
+        
+        if let sound = NSDataAsset(name: soundName){
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("Error: data in \(soundName) couldn't be played as a sound")
+            }
+        }else{
+            print("The sound did not play!")
+        }
+        
 }
-
+    @IBAction func tellMePressed(_ sender: UIButton) {
+        playsound(soundName: "alright", audioPlayer: &audioPlayer)
+    }
+}
